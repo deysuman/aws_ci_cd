89a4a73357dc344f9ccfa52e2b6d436d756e5cdd
Suman Dey <email.sumandey@gmail.com>
2021-08-30 03:28:11 +0530

    Upload project

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const jwt = require('_helpers/jwt');
+// const jwt = require('_helpers/jwt');
 const errorHandler = require('_helpers/error-handler');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // use JWT auth to secure the api
-app.use(jwt());
+//app.use(jwt());
 
 // api routes
 //app.use('/users', require('./users/users.controller'));
