4e1b2cb88916b4284f8eb420353b497b5a9f09fe
Suman Dey <email.sumandey@gmail.com>
2021-08-30 03:16:59 +0530

    Upload project

--- index.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.listen(3000, () =>
-  console.log(`Example app listening on port 3000!`),
-);
