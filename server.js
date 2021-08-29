b681b9e65857f4ffd002e7686c226f34db471e4b
Suman Dey <email.sumandey@gmail.com>
2021-08-30 03:30:26 +0530

    Upload project

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 // const jwt = require('_helpers/jwt');
-const errorHandler = require('_helpers/error-handler');
+// const errorHandler = require('_helpers/error-handler');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -17,7 +17,7 @@ app.use(cors());
 //app.use('/users', require('./users/users.controller'));
 
 // global error handler
-app.use(errorHandler);
+//app.use(errorHandler);
 
 // start server
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
